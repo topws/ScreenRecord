c6a22674b9658d51b8a284704c2524703366cbe6
qianwei <qianwei@admindeMac-mini.local>
2016-04-21 16:47:43 +0800

    1、创建工程

---
+++ ScreenRecoderForGame/ScreenRecoderForGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScreenRecoderForGame
+//
+//  Created by qianwei on 16/4/21.
+//  Copyright © 2016年 qianwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ScreenRecoderForGame/ScreenRecoderForGame/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ScreenRecoderForGame
+//
+//  Created by qianwei on 16/4/21.
+//  Copyright © 2016年 qianwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
